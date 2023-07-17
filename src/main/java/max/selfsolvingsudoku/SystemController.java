abf296af83c6b4ac82ea59ae2f0b17425d3de665
Maxim Subotin <maxim.sub21@gmail.com>
2023-07-17 19:11:06 +0300

    adding the ability to choose a cell, input a number into that cell, input check method.

--- src/main/java/max/selfsolvingsudoku/SystemController.java
+++ src/main/java/max/selfsolvingsudoku/SystemController.java
@@ -1,14 +1,45 @@
 package max.selfsolvingsudoku;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class SystemController {
+
+    TextField activeField = null;
     @FXML
-    private Label welcomeText;
+    protected void onHelloButtonClick(MouseEvent e) {
+        this.resetLabelBorder();
+        activeField = identifyLabel(e);
+        activeField.setStyle("-fx-border-width: 4; -fx-border-color: blue;");
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void enterUserInput(KeyEvent e) {
+        if (activeField != null) {
+            if (checkNumberInput(e.getText())) {
+                activeField.setText(e.getText());
+            }
+        }
+    }
+
+    // Helper Methods
+    private TextField identifyLabel(MouseEvent e) { return (TextField)e.getSource(); }
+
+    private void resetLabelBorder() {
+        if (activeField != null)
+            activeField.setStyle("-fx-border-width: 0;");
+    }
+
+    private boolean checkNumberInput(String input) {
+        if (input.length() != 1) return false;
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
